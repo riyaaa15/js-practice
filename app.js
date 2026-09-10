cc4040424da183664f18d1acfb1341e1e25e010a
riyaaa15 <riyaredhu1020@gmail.com>
2026-09-10 16:48:30 +0530

    add question of leap year

--- app.js
+++ app.js
@@ -340,3 +340,22 @@ const mostFrequentChar = (str) => {
 }
 
 console.log(mostFrequentChar("banana"));
+
+
+// Q31: check if a year is a leap year
+const isLeapYear = (year) => {
+    if(year % 400 === 0) {
+        return true;
+    } else if(year % 100 === 0) {
+        return false;
+    } else if(year % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isLeapYear(2000));  
+console.log(isLeapYear(1900));   
+console.log(isLeapYear(2024));   
+console.log(isLeapYear(2028));
